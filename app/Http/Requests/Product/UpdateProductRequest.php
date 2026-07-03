f35f94b7ec44498e844df7f4c20e7976cc1e61fd
Roihan Al Fahmi <roihanalfahmi123@gmail.com>
2026-07-03 11:11:10 +0700

    add request product

---
+++ app/Http/Requests/Product/UpdateProductRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Product;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateProductRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $productId = $this->route('product')?->id;
+
+        return [
+            'name'        => "required|string|max:255|unique:products,name,{$productId}",
+            'category_id' => 'required|exists:categories,id',
+            'description' => 'nullable|string',
+            'price'       => 'required|numeric|min:0',
+            'stock'       => 'required|integer|min:0',
+            'image'       => 'nullable|image|max:2048',
+            'is_active'   => 'boolean',
+        ];
+    }
+}
